1e4bc4c6aae55e5f0f2bcf0162b580250d4b4c8f
JSDEV1999 <rameshlearningadvance@gmail.com>
2022-09-05 09:41:10 +0530

    first commit

---
+++ 01_Introduction/first.py
@@ -0,0 +1,44 @@
+# introduction
+
+# print('hello, world!')
+
+
+print(2+3) # addittion
+
+print(2-3) # subtraction
+
+print(2*3)
+
+print(2/3) # division(/)
+
+print(3/2) # division(/)
+
+print(2//3)  # Floor division operator(//)
+
+print(3//2)  # Floor division operator(//)
+
+print(2%3)  # modulus(%)
+
+print(3%2)  # modulus(%)
+
+print(2**3)
+
+print(3**2)
+
+# data types
+
+print(type(10))
+
+print(type(4.54))
+
+print(type(1+3j))
+
+print(type('ramesh'))
+
+print(type(['1','2'])) # array / list
+
+print(type({"a": 1, "b": 2})) # object / dict 
+
+print(type({1,5,4,2,8})) # set
+
+print(type((4,8,5,7,9))) # tuple
